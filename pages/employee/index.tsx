0ffcc670ebadb6e9a818de61c328c1ce0c001561
jit rana magar <jitbdrrana8@gmail.com>
2023-02-10 16:26:18 +0545

    fix:add table and button

--- pages/employee/index.tsx
+++ pages/employee/index.tsx
@@ -3,39 +3,39 @@ import Table from "@/components/employee/Table"
 import Mainlayout from "@/layouts/Mainlayout"
 import Link from "next/link"
 
-const index=()=>{
-    return(
-       <Index/>
-    )
+const index = () => {
+  return (
+    <Index />
+  )
 }
 export default index
 
-const breadCrumb:{title:string,link:string}[]=[
-    {
-      title:'Home',
-      link:'/employee'
-    },
-    {
-      title:'Employee',
-      link:'#'
-    },
-  ]
+const breadCrumb: { title: string, link: string }[] = [
+  {
+    title: 'Home',
+    link: '/employee'
+  },
+  {
+    title: 'Employee',
+    link: '#'
+  },
+]
 
-const Index=()=>{
-    return(
-     <Mainlayout title="employee">
+const Index = () => {
+  return (
+    <Mainlayout title="employee">
       <Container
-       breadCrumb={breadCrumb}
-      title="employee"
+        breadCrumb={breadCrumb}
+        title="Employee"
       >
-         <div className="flex justify-end">
-            <Link href={'/employee/create'}>
-             <span className="bg-green-500 text-white hover:bg-green-800 rounded-md px-3 py-2">Add Employee</span>
-            </Link>
-         </div>
-        <Table/>
+        <div className="flex justify-end my-5">
+          <Link href={'/employee/create'}>
+            <span className="bg-green-500 text-white hover:bg-green-800 rounded-md px-3 py-2">Add Employee</span>
+          </Link>
+        </div>
+        <Table />
       </Container>
-     </Mainlayout>
-    )
+    </Mainlayout>
+  )
 
 }
